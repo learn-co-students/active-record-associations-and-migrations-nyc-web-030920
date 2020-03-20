223303e5e556045bd406a598195a62adf01afb35
github username <github email address>
2020-03-20 13:45:00 -0400

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,15 @@
-class CreateSongs < ActiveRecord::Migration[4.2]
+class CreateSongs < ActiveRecord::Migration[5.2]
+    def change
+        create_table :songs do |t|
+            t.string :name
+            t.integer :artist_id
+            t.integer :genre_id
+        end
+    end
 end
+
+#this makes a table called songs that includes
+#2 columns for foreign keys, artist_id and genre_id
+
+#the artist table is going to be the JOIN table
+#songs will connect with genre through artist
